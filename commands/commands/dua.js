5ab16ea1a18b099df9f352a9f7c7c2f1c4f8596d
TARZAN-XD <tkrshqtk@gmail.com>
2025-07-17 09:58:07 +0300

    Delete commands/commands directory

--- commands/commands/dua.js
+++
@@ -1,5 +0,0 @@
-const data = require('../data/dua.json');
-module.exports = function () {
-  const random = Math.floor(Math.random() * data.length);
-  return `🤲 *دعاء اليوم:*\n\n${data[random]}`;
-};
