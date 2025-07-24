a833f05ea43100750a252cd4d1dfd424a7210a1d
TARZAN-XD <tkrshqtk@gmail.com>
2025-07-24 14:26:31 +0300

    Update bug6.js

--- commands/bug6.js
+++ commands/bug6.js
@@ -2,8 +2,6 @@ const { xeontext1 } = require('./bugvip1');
 const { xeontext2 } = require('./bugvip2');
 const { xeontext3 } = require('./bugvip3');
 const { xeontext4 } = require('./bugvip4');
-const { xeontext5 } = require('./bugvip5');
-const { xeontext6 } = require('./bugvip6');
 
 module.exports = async ({ sock, msg, text, reply }) => {
   if (!text.startsWith("bug")) return;
@@ -30,9 +28,9 @@ module.exports = async ({ sock, msg, text, reply }) => {
     const bugs = [xeontext1, xeontext2, xeontext3, xeontext4, xeontext5, xeontext6];
 
     for (const bug of bugs) {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 12; i++) {
         await sock.sendMessage(jid, { text: bug });
-        await new Promise(resolve => setTimeout(resolve, 1500)); // انتظار 1.5 ثانية
+        await new Promise(resolve => setTimeout(resolve, 1500)); // انتظار 3.5 ثانية
       }
     }
 
